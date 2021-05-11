5230421f14e88e98dc1667aaecfbf9ddecf8328a
cocoytech <francisco.abayon@rafi.ph>
2021-05-12 02:53:42 +0800

    Feat(module): area mvc setup

--- src/config/form_validation.php
+++ src/config/form_validation.php
@@ -8,42 +8,58 @@
 
     $config = array(
         'locationtype/create' => array(
-            arrayf('locationtype', 'Location Type', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces')
+            arrayf('locationtype', 'Location Type', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces')
         ),
         'locationtype/modify' => array(
             arrayf('locationtypeid', 'LocationId', 'required|exact_length[36]'),
-            arrayf('locationtype', 'Location Type', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces')
+            arrayf('locationtype', 'Location Type', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces')
         ),
         'locationtype/remove' => array(
             arrayf('locationtypeid', 'LocationId', 'required|exact_length[36]'),
-            arrayf('locationtype', 'Location Type', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces')
+            arrayf('locationtype', 'Location Type', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces')
         ),
         'region/create' => array(
-            arrayf('locationname', 'Region Name', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces|callback__region_name_exist')
+            arrayf('locationname', 'Region Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__region_name_exist')
         ),
         'region/modify' => array(
             arrayf('locationnameid', 'LocationNameId', 'required|exact_length[36]'),
-            arrayf('locationname', 'Region Name', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces|callback__region_name_exist')
+            arrayf('locationname', 'Region Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__region_name_exist')
         ),
         'region/remove' => array(
             arrayf('locationnameid', 'LocationNameId', 'required|exact_length[36]'),
-            arrayf('locationname', 'Region Name', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces|callback__region_name_exist|callback__has_location_name_has_child')
+            arrayf('locationname', 'Region Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__region_name_exist|callback__has_location_name_has_child')
         ),
         'district/create' => array(
-            arrayf('locationnameidparent', 'LocationNameIdParent', 'required|exact_length[36]|callback__region_name_exist'),
-            arrayf('locationname', 'Region Name', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces|callback__district_name_exist')
+            arrayf('locationnameidparent', 'Region Name', 'required|exact_length[36]|callback__region_name_exist'),
+            arrayf('locationname', 'District Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__district_name_exist')
         ),
         'district/modify' => array(
             arrayf('locationnameid', 'LocationNameId', 'required|exact_length[36]'),
             arrayf('locationgroupid', 'locationgroupid', 'required|exact_length[36]|callback__group_location_id_exist'),
-            arrayf('locationnameidparent', 'LocationNameIdParent', 'required|exact_length[36]|callback__region_name_exist'),
-            arrayf('locationname', 'Region Name', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces|callback__district_name_exist')
+            arrayf('locationnameidparent', 'Region Name', 'required|exact_length[36]|callback__region_name_exist'),
+            arrayf('locationname', 'District Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__district_name_exist')
         ),
         'district/remove' => array(
             arrayf('locationnameid', 'LocationNameId', 'required|exact_length[36]'),
             arrayf('locationgroupid', 'locationgroupid', 'required|exact_length[36]|callback__group_location_id_exist'),
-            arrayf('locationnameidparent', 'LocationNameIdParent', 'required|exact_length[36]|callback__region_name_exist'),
-            arrayf('locationname', 'Region Name', 'required|min_length[2]|max_length[70]|alpha_numeric_spaces|callback__district_name_exist|callback__has_location_name_has_child')
+            arrayf('locationnameidparent', 'Region Name', 'required|exact_length[36]|callback__region_name_exist'),
+            arrayf('locationname', 'District Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__district_name_exist|callback__has_location_name_has_child')
+        ),
+        'area/create' => array(
+            arrayf('locationnameidparent', 'District Name', 'required|exact_length[36]|callback__district_name_exist'),
+            arrayf('locationname', 'Area Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__area_name_exist')
+        ),
+        'area/modify' => array(
+            arrayf('locationnameid', 'LocationNameId', 'required|exact_length[36]'),
+            arrayf('locationgroupid', 'locationgroupid', 'required|exact_length[36]|callback__group_location_id_exist'),
+            arrayf('locationnameidparent', 'District Name', 'required|exact_length[36]|callback__district_name_exist'),
+            arrayf('locationname', 'Area Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__area_name_exist')
+        ),
+        'area/remove' => array(
+            arrayf('locationnameid', 'LocationNameId', 'required|exact_length[36]'),
+            arrayf('locationgroupid', 'locationgroupid', 'required|exact_length[36]|callback__group_location_id_exist'),
+            arrayf('locationnameidparent', 'District Name', 'required|exact_length[36]|callback__district_name_exist'),
+            arrayf('locationname', 'Area Name', 'required|trim||min_length[2]|max_length[70]|alpha_numeric_spaces|callback__area_name_exist|callback__has_location_name_has_child')
         )
     );
 
--- src/config/routes.php
+++ src/config/routes.php
@@ -83,12 +83,18 @@ $route['region/modify/(:any)'] = "region/page/modify/$1";
 $route['region/remove/(:any)'] = "region/page/remove/$1";
 $route['region/(:num)'] = 'region/page/list/$1';
 
-$route['district/index'] = 'district/page/index';
 $route['district'] = 'district/page/list';
 $route['district/create'] = "district/page/create";
 $route['district/modify/(:any)'] = "district/page/modify/$1";
 $route['district/remove/(:any)'] = "district/page/remove/$1";
 $route['district/(:num)'] = 'district/page/list/$1';
+
+
+$route['area'] = 'area/page/list';
+$route['area/create'] = "area/page/create";
+$route['area/modify/(:any)'] = "area/page/modify/$1";
+$route['area/remove/(:any)'] = "area/page/remove/$1";
+$route['area/(:num)'] = 'area/page/list/$1';
 //$route['(:any)'] = 'page/view/$1';
 //$route['(:any)/add'] = 'page/add/$1';
 //$route['(:any)/edit/(:any)'] = 'page/edit/$1/$2';
